f6daebca0dd63425041d050808d816f6b7836957
Sherrie Velasquez <sherrie.velas@gmail.com>
2022-09-25 07:57:59 -0700

    js functions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-// create our questions
-// arr of objects; including questions: store possible answers and the correct answer
+// variables and DOM objects
 var contentEl = document.getElementById("content");
-var startBtn = document.querySelector("#start"); //take a css style el selector
+var startBtn = document.querySelector("#start");
 var timerEl = document.querySelector("#timer");
+var detailsEl = document.querySelector("#details");
 var containerEl = document.querySelector("#container");
-var timerCount = 5;
+var timerCount = 100;
 var currentQuestIndex = 0;
 var outcome = document.querySelector("#answer-outcome");
-
-const scorePoints = 25;
-
-var gameOver = document.getElementById("#quiz-end");
+var submitBtn = document.querySelector("#submit-btn");
+var timerInterval;
+var bodyEl = document.body;
+var gameOver = document.querySelector(".quiz-end");
 var winGame = document.querySelector("#content");
+var submitBtn = document.querySelector("#submit-btn");
 
+// create questions
 var questions = [
   {
     question: "Commonly used data types do NOT include: ",
@@ -53,7 +55,10 @@ var questions = [
     answer: "1. console.log",
   },
 ];
+
+// generate questions container
 function renderCurrentQuest() {
+  containerEl.setAttribute = ("style", "display:none");
   containerEl.innerHTML = "";
   var currentQuest = questions[currentQuestIndex];
 
@@ -72,44 +77,89 @@ function renderCurrentQuest() {
   containerEl.appendChild(ulEl);
 }
 
+//function click "start quiz" button to start quiz
 startBtn.addEventListener("click", function () {
+  timerEl.textContent = timerCount;
   renderCurrentQuest();
   startBtn.setAttribute("style", "display: none;");
   startBtn.innerHTML = "";
 
-  var timerInterval = setInterval(function () {
+  // timer function
+  timerInterval = setInterval(function () {
     timerEl.textContent = timerCount;
     timerCount--;
-    if (timerCount === 0) {
-      clearInterval(timerInterval);
-      // change DOM to say "Game Over!"
+    if (timerCount <= 0) {
       endGame();
     }
   }, 1000);
 });
-var bodyEl = document.body;
+
+// calculate time remaining and replace with score
+if (timerCount >= 0) {
+  var timeRemaining = timerCount;
+  var finalScoreTxt = document.createElement("p");
+  clearInterval(timerInterval);
+  finalScoreTxt.textContent = "Your final score is: " + timeRemaining;
+
+  contentEl.appendChild(finalScoreTxt);
+}
+// end of quiz, directs user to view/store highscores
+
+// var finalScr = document.getElementById("final-score");
 
 function endGame() {
-  gameOver.setAttribute("style", "display: box;");
+  clearInterval(timerInterval);
+  contentEl.setAttribute("style", "display: none;");
+  contentEl.innerHTML = "";
+  outcome.setAttribute("style", "display: none;");
+  outcome.innerHTML = "";
+  gameOver.classList.remove("quiz-end");
+  document.getElementById("final-score").textContent = timerCount;
   bodyEl.appendChild(gameOver);
 }
 
-containerEl.addEventListener("click", function (event) {
-  // console.log(event);
+// input initials and local storage for initials and score
+submitBtn.addEventListener("click", function () {
+  // var initials = document.getElementById("name").value;
+  var initials = createInput.value;
+  if (initials !== "") {
+    var finalScore = {
+      initials: initials,
+      score: timeRemaining,
+    };
+    console.log(finalScore);
+    var scores = JSON.parse(localStorage.getItem("finalScore")) || [];
+    scores.push(finalScore);
+    var newScore = JSON.stringify(scores);
+    localStorage.setItem("scores", newScore);
+    // localStorage.setItem("finalScr", JSON.stringify(scores));
+    // redirect user to highscores page
+    window.location.href = "./highscores.html";
+  }
+});
 
+//answer quiz questions
+containerEl.addEventListener("click", function (event) {
+  // console.log(event); test
   if (event.target.matches("li")) {
     var currentQuest = questions[currentQuestIndex];
 
     var userGuess = event.target.textContent;
     console.log(userGuess);
-
     if (userGuess === currentQuest.answer) {
       outcome.textContent = "Correct!";
       document.body.appendChild(outcome);
-      //   increase score
-      scorePoints;
-      // modify timer
+      // score increase
+      timerCount += 2;
+      // modify timer; penalty for wrong answer
     } else {
+      timerCount -= 10;
+      if (timerCount === 0) {
+        endGame();
+      }
+      if (currentQuestIndex === questions.length - 1) {
+        endGame();
+      }
       outcome.textContent = "Wrong!";
       document.body.appendChild(outcome);
     }
@@ -118,4 +168,3 @@ containerEl.addEventListener("click", function (event) {
   }
 });
 // quiz-finish, enter initials
-// input and view highsores local storage
